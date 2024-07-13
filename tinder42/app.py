206c0efe1ded1acfdcaf2f33e3637c5119e1d472
Enes <nsyagz@gmail.com>
2024-07-13 12:38:27 +0300

    chore: Add signup page and update login page

--- tinder42/app.py
+++ tinder42/app.py
@@ -4,9 +4,13 @@ from flask import render_template
 app = Flask(__name__)
 
 @app.route('/')
+@app.route('/login')
 def index():
     return render_template('login.html')
 
+@app.route('/signup')
+def signup():
+    return render_template('signup.html')
+
 if __name__ == '__main__':
-    app.run(debug=True)
- 
+    app.run(debug=True, port=5041)
